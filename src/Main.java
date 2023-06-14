67b6771115845cf9b2931e062dd600d3e963b05c
Melany Barrera <Melani_nayhely@outlook.es>
2023-06-13 22:52:42 -0500

    Creacion del Main basado en trabajo de Equipo 2 - Hiperbole agregada

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import conicas2.*;
+
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
 /*Esta clase contiene un menú compuesto por el package del equipo 2, realizado por todos los integrantes del equipo 1
@@ -24,20 +26,22 @@ public class Main {
         //Circunferencia
 
         //Elipse
+        hiperbole figura1 = new hiperbole(2.63, 5.78);
 
         //Creamos la pantalla del menu principal
-        System.out.println("------------------------------------Figuras 3D con Package Equipo 2----------------------------------");
+        System.out.println("\n\t------------------------------------Figuras 3D con Package Equipo 2----------------------------------");
 
         System.out.println("\nEl presente sistema permite al usuario el calcular diferentes operaciones de cónicas, mediante el empleo" +
                             "de un menu y selección. \nLas conicas que se ofrecen son:");
 
         System.out.println("\n----> a. Parabola");
-        System.out.println("\n----> b. Hiperbole");
-        System.out.println("\n----> c. Circunferencia");
-        System.out.println("\n----> d. Elipse");
+        System.out.println("----> b. Hiperbole");
+        System.out.println("----> c. Circunferencia");
+        System.out.println("----> d. Elipse");
 
         System.out.println("\nPara continuar, seleccione la letra de la conica a usar: ");
         String opt = entrada.next();
+        
 
         switch (opt){
             case ("a"):
@@ -45,7 +49,9 @@ public class Main {
                 break;
 
             case ("b"):
-
+                System.out.println("\n-----> Ha seleccionado Hiperbole");
+                System.out.println("\n\t-----> Para primera instancia: ");
+                figura1.variables();
                 break;
 
             case ("c"):
@@ -57,7 +63,7 @@ public class Main {
                 break;
 
             default:
-                
+                System.out.println("----> ERROR! Esta conica no se encuentra en el sistema!");
                 break;
         }
 
